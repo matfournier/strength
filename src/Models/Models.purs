73e44ad514a6e6cae0ac56c029baa1a293b7aba7
Mathew Fournier <mat@matfournier.com>
2018-04-10 21:39:30 -0700

    shuffling stuff around

--- src/Models/Models.purs
+++ src/Models/Models.purs
@@ -7,6 +7,7 @@ import Data.Int (toNumber)
 import Degree (Deg, deg2rad, rad2deg)
 import Math (tan, asin, pow, sqrt, exp, e)
 import Data.Tuple (Tuple(..), fst, snd)
+import MF.Models.HoekBrown
 
 -- | Generic aliasing
 
@@ -22,22 +23,9 @@ newtype FrictionalStrengthModel = FrictionalStrength {phi :: Deg}
 newtype ShearNormalModel = ShearNormalModel { normalStress :: Array Number, shearStress :: Array Number }
 
 instance showShearNormalModel :: Show ShearNormalModel where
-  show (ShearNormalModel m) = "shear stress: " <> show m.shearStress <> "\n" 
-    <> "normal stress: " <> show m.normalStress 
+  show (ShearNormalModel m) = "normal stress: " <> show m.normalStress <> "\n" 
+    <> "shear stress: " <> show m.shearStress 
 
-type HoekBrown = { name :: String 
-                 , ucs :: Number
-                 , gsi :: Number
-                 , mi :: Number
-                 , d :: Number
-                 , ei :: Number }
-
-newtype HoekBrownModel = HoekBrownModel { inputs :: HoekBrown
-                                        , mb :: Number
-                                        , s :: Number
-                                        , a :: Number
-                                        , ucs' :: Number
-                                        , t' :: Number }
 
 hoekBrownToMohrColoumb :: HoekBrownModel -> Number -> MohrColoumbModel
 hoekBrownToMohrColoumb (HoekBrownModel hb) sig3max = MohrColoumbModel {phi: rad2deg p, cohesion: c}
@@ -110,11 +98,6 @@ instance mcShearNormal :: ShearNormal MohrColoumbModel ShearNormalParameters whe
       ss = ns # map (\s -> tan(deg2rad(mc.phi)) + mc.cohesion)   
 
 
--- | todo move to util functions
-normalStressRange :: Number -> Number -> Int -> Array Number 
-normalStressRange min max granularity =
-  let rangeStep =(max - min) / toNumber(granularity)
-      in map (\v -> min + (toNumber(v) * rangeStep)) $ range 1 granularity
 
 -- | todo add in the tensile strength (-ve normal stress) rang 
 instance hbShearNormal :: ShearNormal HoekBrownModel ShearNormalParameters where
@@ -127,50 +110,3 @@ instance hbShearNormal :: ShearNormal HoekBrownModel ShearNormalParameters where
       stresses = s3range # map (\s3 -> hbGenerateShearNormal hb s3) # unzip 
 
 
--- calculate the reduced value of the material constant mi 
-mb :: HoekBrown -> Number
-mb hb = hb.mi * exp ((hb.gsi - 100.0) / (28.0 - 14.0 * hb.d))
-
--- calculate s rock mass constant
-s :: HoekBrown -> Number
-s hb = exp ((hb.gsi - 100.0) / (9.0 - 3.0 * hb.d))
-
--- calculate a rock mass constant
-a :: HoekBrown -> Number
-a hb = 0.5 + (1.0 / 6.0) * (pow e (-hb.gsi / 15.0) ) - (pow e (-20.0 / 3.0))
-
--- calculate uniaxial compressive strength
-ucs' :: HoekBrown -> Number -> Number -> Number
-ucs' hb s a = hb.ucs * pow s a 
-
--- calculate tensile strength
-
-t' :: HoekBrown -> Number -> Number -> Number
-t' hb s mb = s * hb.ucs / mb
-
--- calculated sigma1' 
-hbSigma1' :: HoekBrownModel -> Number -> Number
-hbSigma1' (HoekBrownModel hb) sig3' =
-      sig3' + hb.inputs.ucs * pow((hb.mb * sig3' / hb.inputs.ucs) + hb.s)(hb.a)
-
--- calculate d'sigma1' / d'sigma3'
-ds1ds3 :: HoekBrownModel -> Number -> Number -> Number
-ds1ds3 (HoekBrownModel hb) s1 s3 = 1.0 +
-                                   (hb.a * hb.mb * pow ((hb.mb * s3)/(hb.ucs' + hb.s)) (hb.a - 1.0))
-
--- calculate  sigma normal'
-hbNormal' :: Number -> Number -> Number -> Number
-hbNormal' s1' s3' ds1ds3 = ((s1' + s3') / 2.0) + ((s1' - s3') / 2.0) * ((ds1ds3 - 1.0) / (ds1ds3 + 1.0))
-
--- calculate tau shear stress
-hbTau :: Number -> Number -> Number -> Number
-hbTau s1' s3' ds1ds3 = (s1' - s3') * sqrt(ds1ds3) / (ds1ds3 + 1.0)
-
--- calculate a shear normal stress pair for a given model and sigma 3 value
-
-hbGenerateShearNormal :: HoekBrownModel -> Number -> Tuple Number Number 
-hbGenerateShearNormal hb sig3' = let sig1' = hbSigma1' hb sig3'
-                                     ds1ds3' = ds1ds3 hb sig1' sig3'
-                                     normalStress = hbNormal' sig1' sig3' ds1ds3'
-                                     shearStress = hbTau sig1' sig3' ds1ds3'
-                                 in Tuple normalStress shearStress 
